1e70b4b924e36a12f6cbdd402f63ecaafc3d0dbb
freekmurze <freekmurze@users.noreply.github.com>
2024-12-11 09:31:37 +0000

    Fix styling

--- src/PendingRouteTransformers/AddDefaultRouteName.php
+++ src/PendingRouteTransformers/AddDefaultRouteName.php
@@ -31,11 +31,10 @@ class AddDefaultRouteName implements PendingRouteTransformer
             ->reject(fn (string $segment) => str_starts_with($segment, '{'))
             ->when(
                 $this->discoverMethodRouteName($pendingRouteAction),
-                fn(Collection $collection, $name) => $name != $collection->last() ? $collection->push($name) : $collection
+                fn (Collection $collection, $name) => $name != $collection->last() ? $collection->push($name) : $collection
             )
             ->join('.');
     }
-
 
     /**
      * @param PendingRouteAction $pendingRouteAction
