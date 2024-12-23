95856314ed6a1a6ead65a64fe90c005711a6b40b
Lukas <39046617+lwlook@users.noreply.github.com>
2024-12-23 13:31:11 +0100

    Added test cases for laravel middleware method discovery

---
+++ tests/Support/TestClasses/Controllers/DoNotDiscoverMiddleware/DiscoverMiddlewareIfNotLaravelMethodController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Spatie\RouteDiscovery\Tests\Support\TestClasses\Controllers\DoNotDiscoverMiddleware;
+
+class DiscoverMiddlewareIfNotLaravelMethodController
+{
+    public static function middleware(): array {
+        return [];
+    }
+
+    public function method()
+    {
+    }
+
+}
---
+++ tests/Support/TestClasses/Controllers/DoNotDiscoverMiddleware/DoNotDiscoverMiddlewareController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Spatie\RouteDiscovery\Tests\Support\TestClasses\Controllers\DoNotDiscoverMiddleware;
+
+use Illuminate\Routing\Controllers\HasMiddleware;
+
+class DoNotDiscoverMiddlewareController implements HasMiddleware
+{
+    public static function middleware(): array {
+        return [];
+    }
+
+    public function method()
+    {
+    }
+
+}
